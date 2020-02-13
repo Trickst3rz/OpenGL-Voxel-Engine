896404b7d0ea1391c8c29cc3dc51e12146d4267c
THOMAS <S6099298@live.tees.ac.uk>
2020-02-13 17:27:21 +0000

    Moved cpp files to source and added vbo to render a square with two triangles

--- Procedural-Terrain/resources/shaders/Basic.shader
+++ Procedural-Terrain/resources/shaders/Basic.shader
@@ -15,5 +15,5 @@ layout(location = 0) out vec4 color;
 
 void main()
 { // colour is 0-1 aka 0-255
-	color = vec4(1.0, 0.0, 0.0, 1.0);
+	color = vec4(0.0, 1.0, 0.0, 1.0);
 };
--- Procedural-Terrain/src/Application.cpp
+++ Procedural-Terrain/src/Application.cpp
@@ -115,20 +115,30 @@ int main(void)
 
 	std::cout << glGetString(GL_VERSION) << std::endl;
 
-	float positions[6] = {
-		-0.5f, -0.5f,  
-		 0.5f, -0.5f,
-		 0.5f,  0.5f
+	float positions[] = {
+		-0.5f, -0.5f,	//0
+		 0.5f, -0.5f,	//1
+		 0.5f,  0.5f,	//2
+		-0.5f,  0.5f,	//3
+	};
 
+	unsigned int indices[] = {
+		0, 1, 2,
+		2, 3, 0
 	};
 
 	unsigned int buffer;
 	glGenBuffers(1, &buffer);
 	glBindBuffer(GL_ARRAY_BUFFER, buffer);
-	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, 4 * 2 * sizeof(float), positions, GL_STATIC_DRAW);
 
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+
+	unsigned int vbo;
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
 
 	ShaderProgramSource source = ParseShader("resources/shaders/Basic.shader");
 
@@ -143,7 +153,7 @@ int main(void)
 		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
 
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
